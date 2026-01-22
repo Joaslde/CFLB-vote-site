08e5ab2f4b3f1f001ea642407d6f88b419476ff3
TonNomGitHub <tonemail@gmail.com>
2026-01-22 01:00:47 +0100

    Try to repaire deployement routing bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,18 @@
 <template>
-    salut
+      <center><h1>salut</h1> </center> 
+
+    <button class="back-button" @click="goBack">
+      <i class="bi bi-chevron-left"></i>
+    </button>
   </template>
+
+  <script setup>
+
+    const goBack = () => {
+  router.push('/candidates').then(() => {
+    router.go(0); // Force un rafraîchissement si nécessaire
+  });
+};
+
+
+  </script>
